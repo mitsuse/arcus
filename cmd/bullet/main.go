d3ef198ee906175c86b8ed873b305cc44351adb5
Tomoya Kose <tomoya@mitsuse.jp>
2015-02-11 22:12:44 +0900

    Initialize this repository.

---
+++ cmd/bullet/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := initApp()
+	app.Run(os.Args)
+}
+
+func initApp() *cli.App {
+	app := cli.NewApp()
+
+	app.Name = "bullet"
+	app.Version = "0.0.1"
+	app.Usage = "A command-line tool to push text or files to Pushbullet."
+
+	return app
+}
