7f589c5109eb111885c70cf0e042fb26d9a4645d
Tomoya Kose <tomoya@mitsuse.jp>
2015-02-12 01:48:56 +0900

    Implement a part of "pushes" method.

--- pushbullet/pushbullet.go
+++ pushbullet/pushbullet.go
@@ -1,6 +1,9 @@
 package pushbullet
 
 import (
+	"bytes"
+	"net/http"
+
 	"github.com/mitsuse/bullet/pushbullet/pushes"
 )
 
@@ -17,6 +20,29 @@ func New(token string) *Pushbullet {
 }
 
 func (pb *Pushbullet) PostNote(n *pushes.Note) error {
-	// TODO: Implement this.
+	buffer := &bytes.Buffer{}
+
+	if err := n.Dump(buffer); err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", ENDPOINT_PUSHES, buffer)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(pb.token, "")
+
+	// TODO: Set the timeout.
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Process an error response.
+
 	return nil
 }
--- pushbullet/pushes/note.go
+++ pushbullet/pushes/note.go
@@ -1,31 +1,50 @@
 package pushes
 
+import (
+	"encoding/json"
+	"io"
+)
+
 const (
 	TYPE_NOTE = "note"
 )
 
 type Note struct {
-	title string
-	body  string
+	jsonObj *noteJson
 }
 
 func NewNote(title, body string) *Note {
+	jsonObj := &noteJson{
+		Type:  TYPE_NOTE,
+		Title: title,
+		Body:  body,
+	}
+
 	n := &Note{
-		title: title,
-		body:  body,
+		jsonObj: jsonObj,
 	}
 
 	return n
 }
 
+func (n *Note) Dump(writer io.Writer) error {
+	return json.NewEncoder(writer).Encode(n.jsonObj)
+}
+
 func (n *Note) Type() string {
-	return TYPE_NOTE
+	return n.jsonObj.Type
 }
 
 func (n *Note) Title() string {
-	return n.title
+	return n.jsonObj.Title
 }
 
 func (n *Note) Body() string {
-	return n.body
+	return n.jsonObj.Body
+}
+
+type noteJson struct {
+	Type  string `json:"type"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
