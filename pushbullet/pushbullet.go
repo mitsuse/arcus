afa919015fa32a9cde48bae5e24968792f4bc01c
Tomoya Kose <tomoya@mitsuse.jp>
2015-02-13 00:15:17 +0900

    Implement "link" subcommand based on the copy of "note" subcommand.

--- pushbullet/pushbullet.go
+++ pushbullet/pushbullet.go
@@ -50,3 +50,34 @@ func (pb *Pushbullet) PostNote(n *pushes.Note) error {
 
 	return nil
 }
+
+func (pb *Pushbullet) PostLink(n *pushes.Link) error {
+	buffer := &bytes.Buffer{}
+
+	if err := n.Dump(buffer); err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", ENDPOINT_PUSHES, buffer)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(pb.token, "")
+
+	// TODO: Set the timeout.
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Return an error value with human friendly message.
+	if res.StatusCode != 200 {
+		return errors.New(res.Status)
+	}
+
+	return nil
+}
