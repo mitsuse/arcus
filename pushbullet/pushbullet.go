6bdfc368b906843398a0e5fca82881c1e7de9726
Tomoya Kose <tomoya@mitsuse.jp>
2015-02-14 16:34:10 +0900

    Move methods for pushes API into "pushes.go".

--- pushbullet/pushbullet.go
+++ pushbullet/pushbullet.go
@@ -12,14 +12,6 @@ See the API documentation for the details: https://docs.pushbullet.com/#http
 */
 package pushbullet
 
-import (
-	"bytes"
-	"errors"
-	"net/http"
-
-	"github.com/mitsuse/bullet/pushbullet/pushes"
-)
-
 type Pushbullet struct {
 	token string
 }
@@ -37,55 +29,4 @@ func New(token string) *Pushbullet {
 	}
 
 	return pb
-}
-
-// Push a note, which consists of "title" and "message" strings.
-func (pb *Pushbullet) PostPushesNote(n *pushes.Note) error {
-	return pb.postPushes(n)
-}
-
-// Push a link, which consists of "title", "message" and "url" strings.
-func (pb *Pushbullet) PostPushesLink(l *pushes.Link) error {
-	return pb.postPushes(l)
-}
-
-// Push an address, which consists of the place "name" and "address (searchquery)" for map.
-func (pb *Pushbullet) PostPushesAddress(a *pushes.Address) error {
-	return pb.postPushes(a)
-}
-
-// Push a checklist, which consists of "title" and the list of items.
-func (pb *Pushbullet) PostPushesCheck(c *pushes.Check) error {
-	return pb.postPushes(c)
-}
-
-func (pb *Pushbullet) postPushes(p pushes.Push) error {
-	buffer := &bytes.Buffer{}
-
-	if err := p.Dump(buffer); err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", ENDPOINT_PUSHES, buffer)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.SetBasicAuth(pb.token, "")
-
-	// TODO: Set the timeout.
-	client := &http.Client{}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	// TODO: Return an error value with human friendly message.
-	if res.StatusCode != 200 {
-		return errors.New(res.Status)
-	}
-
-	return nil
 }
