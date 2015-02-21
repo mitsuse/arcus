c2f3b7836d418c24186b333cb8ffd5d6e66b82f5
Tomoya Kose <tomoya@mitsuse.jp>
2015-02-21 16:08:07 +0900

    Update the version number.

--- commands/commands.go
+++ commands/commands.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	NAME         = "bullet"
-	VERSION      = "0.1.0"
+	VERSION      = "0.1.2"
 	DESC         = "A command-line tool to send a message to devices via Pushbullet."
 	AUTHOR       = "Tomoya Kose (mitsuse)"
 	AUTHOR_EMAIL = "tomoya@mitsuse.jp"
